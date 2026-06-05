ff63cd550c59cb8749e217ca9462de519402242d
Liam Clarke <liam.clarke@circleci.com>
2026-06-05 12:34:55 +0100

    Add testsuite example for using pytest classnames
    - `discover` reutrns a list of test atom class IDs.
    ```
    tests/test_pytest_circleci_coverage.py::test_pytest_sessionfinish_success
    ```
    
    - `run` supports running these natively.
    - `file-mapper` strips the trailing `::` to leave a file path.
    ```
    tests/test_pytest_circleci_coverage.py
    ```
    - `analysis` supports running these natively.
    - `conftest` uses `record_xml_attribute` to set the classname in JUnit.
    
    https://docs.pytest.org/en/stable/how-to/output.html#record-xml-attribute

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,11 @@
+import pytest
+
 pytest_plugins = ["pytester", "pytest_circleci_coverage"]
+
+
+## Fixture to set the classname attribute in the junit xml report,
+## so that it matches the test atoms discovered and run in the
+## test-suites.yml file.
+@pytest.fixture(autouse=True)
+def _circleci_classname(request, record_xml_attribute):
+    record_xml_attribute("classname", request.node.nodeid)
